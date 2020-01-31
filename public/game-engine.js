ccd2209400dc4c5a4bb080965b172dac2ae47369
Massimo Avvisati <dw@mondonerd.com>
2020-01-31 23:05:42 +0100

    Simple movement and proof concept for the camera movement

--- public/game-engine.js
+++ public/game-engine.js
@@ -13,3 +13,33 @@ const Game = {
   }
 
 }
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  var playerElement = document.querySelector('#background');
+
+  var move = function(direction) {
+    var boundingBox = playerElement.getBoundingClientRect();
+    var increment = (direction == 'bottom' || direction == 'right') ? -1 : 1;
+    var axis = (direction == 'top' || direction == 'bottom') ? 'y' : 'x';
+    var styleDirection = direction == 'left' || direction == 'right' ? 'left' : 'top';
+    playerElement.style[styleDirection] = (boundingBox[axis] + (50 * increment)) + 'px';
+
+  }
+  document.querySelector('#down').addEventListener('click', () => {
+    move('bottom')
+  })
+
+  document.querySelector('#up').addEventListener('click', () => {
+    move('top')
+  })
+
+  document.querySelector('#left').addEventListener('click', () => {
+    move('left')
+  })
+
+  document.querySelector('#right').addEventListener('click', () => {
+    move('right')
+  })
+
+})
