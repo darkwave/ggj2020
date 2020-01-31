16482460a0ffc92d724d3aef52e87d10e1b20da6
Massimo Avvisati <dw@mondonerd.com>
2020-02-01 00:28:39 +0100

    Broken collision

--- public/game-engine.js
+++ public/game-engine.js
@@ -18,12 +18,29 @@ const Game = {
 document.addEventListener("DOMContentLoaded", function() {
   var playerElement = document.querySelector('#background');
 
+  var playerLayer = document.querySelector('#player_layer');
+
+  var playerX = playerLayer.getBoundingClientRect().width / 2;
+  var playerY = playerLayer.getBoundingClientRect().height / 2;
   var move = function(direction) {
     var boundingBox = playerElement.getBoundingClientRect();
     var increment = (direction == 'bottom' || direction == 'right') ? -1 : 1;
     var axis = (direction == 'top' || direction == 'bottom') ? 'y' : 'x';
     var styleDirection = direction == 'left' || direction == 'right' ? 'left' : 'top';
-    playerElement.style[styleDirection] = (boundingBox[axis] + (50 * increment)) + 'px';
+    playerElement.style[styleDirection] = (boundingBox[axis] + (64 * increment)) + 'px';
+
+    var objs = document.querySelectorAll('.objs');
+    objs.forEach(obj => {
+      objPos = obj.getBoundingClientRect()
+      if (Math.abs(playerX - objPos.x) < 32 && Math.abs(playerY - objPos.y) < 32) {
+        obj.style.backgroundColor = 'red'
+      } else {
+        obj.style.backgroundColor = ''
+
+      }
+    })
+
+    //console.log(document.querySelector('#obj').getBoundingClientRect());
 
   }
   document.querySelector('#down').addEventListener('click', () => {
@@ -40,6 +57,18 @@ document.addEventListener("DOMContentLoaded", function() {
 
   document.querySelector('#right').addEventListener('click', () => {
     move('right')
+  })
+
+  document.addEventListener('keyup', function(event) {
+    if (event.keyCode == 38) {
+      move('top');
+    } else if (event.keyCode == 40) {
+      move('bottom');
+    } else if (event.keyCode == 39) {
+      move('right');
+    } else if (event.keyCode == 37) {
+      move('left');
+    }
   })
 
 })
