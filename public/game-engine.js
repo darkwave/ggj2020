bc0a05825af246ffbc09bb6733bc5319bd17de0a
Saeen-Damghanian <saeen.damghanian@gmail.com>
2020-02-01 09:54:30 +0100

    tryin to fix collider Saeen#1

--- public/game-engine.js
+++ public/game-engine.js
@@ -1,74 +1,79 @@
 const Game = {
   entities: [],
-  addEntity: function(e) {
-
-  },
-  run: function() {
-
-  },
+  addEntity: function(e) {},
+  run: function() {},
   render: function() {
-    entities.forEach(e => {
-
-    })
+    entities.forEach(e => {});
   }
-
-}
-
+};
 
 document.addEventListener("DOMContentLoaded", function() {
-  var playerElement = document.querySelector('#background');
+  var island = document.querySelector("#background");
+  var playerElement = document.querySelector("#player");
 
-  var playerLayer = document.querySelector('#player_layer');
+  var playerLayer = document.querySelector("#player_layer");
 
-  var playerX = playerLayer.getBoundingClientRect().width / 2;
-  var playerY = playerLayer.getBoundingClientRect().height / 2;
+  var screenCenterX = playerLayer.getBoundingClientRect().width / 2;
+  var screenCenterY = playerLayer.getBoundingClientRect().height / 2;
   var move = function(direction) {
-    var boundingBox = playerElement.getBoundingClientRect();
-    var increment = (direction == 'bottom' || direction == 'right') ? -1 : 1;
-    var axis = (direction == 'top' || direction == 'bottom') ? 'y' : 'x';
-    var styleDirection = direction == 'left' || direction == 'right' ? 'left' : 'top';
-    playerElement.style[styleDirection] = (boundingBox[axis] + (64 * increment)) + 'px';
+    var islandBoundingBox = island.getBoundingClientRect();
+    var playerBoundingBox = playerElement.getBoundingClientRect();
+    var increment = direction == "bottom" || direction == "right" ? -1 : 1;
+    var axis = direction == "top" || direction == "bottom" ? "y" : "x";
+    var styleDirection =
+      direction == "left" || direction == "right" ? "left" : "top";
+    island.style[styleDirection] =
+      islandBoundingBox[axis] + 64 * increment + "px";
 
-    var objs = document.querySelectorAll('.objs');
-    objs.forEach(obj => {
-      objPos = obj.getBoundingClientRect()
-      if (Math.abs(playerX - objPos.x) < 32 && Math.abs(playerY - objPos.y) < 32) {
-        obj.style.backgroundColor = 'red'
+    var objs = document.querySelectorAll(".objs");
+    objs.forEach((obj, index) => {
+      objPos = obj.getBoundingClientRect();
+      console.log(
+        `object ${index}: ${objPos.x} ${objPos.y} playerX: ${
+          playerBoundingBox.x
+        } playerY : ${playerBoundingBox.y} disX: ${Math.abs(
+          playerBoundingBox.x - objPos.x
+        )} disY: ${Math.abs(
+          playerBoundingBox.y - objPos.y
+        )} Screen X,Y: ${screenCenterX},${screenCenterY}`
+      );
+      if (
+        Math.abs(playerBoundingBox.x - objPos.x) < 32 &&
+        Math.abs(playerBoundingBox.y - objPos.y) < 32
+      ) {
+        obj.style.backgroundColor = "red";
       } else {
-        obj.style.backgroundColor = ''
-
+        obj.style.backgroundColor = "";
       }
-    })
+    });
 
     //console.log(document.querySelector('#obj').getBoundingClientRect());
+  };
+  document.querySelector("#down").addEventListener("click", () => {
+    move("bottom");
+  });
 
-  }
-  document.querySelector('#down').addEventListener('click', () => {
-    move('bottom')
-  })
+  document.querySelector("#up").addEventListener("click", () => {
+    move("top");
+  });
 
-  document.querySelector('#up').addEventListener('click', () => {
-    move('top')
-  })
+  document.querySelector("#left").addEventListener("click", () => {
+    move("left");
+  });
 
-  document.querySelector('#left').addEventListener('click', () => {
-    move('left')
-  })
+  document.querySelector("#right").addEventListener("click", () => {
+    move("right");
+  });
 
-  document.querySelector('#right').addEventListener('click', () => {
-    move('right')
-  })
-
-  document.addEventListener('keyup', function(event) {
+  document.addEventListener("keyup", function(event) {
     if (event.keyCode == 38) {
-      move('top');
+      move("top");
     } else if (event.keyCode == 40) {
-      move('bottom');
+      move("bottom");
     } else if (event.keyCode == 39) {
-      move('right');
+      move("right");
     } else if (event.keyCode == 37) {
-      move('left');
+      move("left");
     }
-  })
-
-})
+  });
+});
