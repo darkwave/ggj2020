6863ac32fb0f823d80a005916b15816e22f465c9
milleymilli <milleylove143@gmail.com>
2020-02-01 17:20:22 +0100

    Adding Digital Clock

--- public/game-engine.js
+++ public/game-engine.js
@@ -8,127 +8,147 @@ const Game = {
 };
 
 document.addEventListener('DOMContentLoaded', function() {
+  var lastTimestamp = 0;
 
-      var lastTimestamp = 0;
+  var movement = null;
+  var island = document.querySelector('#background');
+  var playerElement = document.querySelector('#player');
 
-      var movement = null;
-      var island = document.querySelector('#background');
-      var playerElement = document.querySelector('#player');
+  var playerLayer = document.querySelector('#player_layer');
 
-      var playerLayer = document.querySelector('#player_layer');
+  var move = function(direction) {
+    var islandBoundingBox = island.getBoundingClientRect();
+    var playerBoundingBox = playerElement.getBoundingClientRect();
+    var increment = direction == 'bottom' || direction == 'right' ? -1 : 1;
+    var axis = direction == 'top' || direction == 'bottom' ? 'y' : 'x';
+    var styleDirection =
+      direction == 'left' || direction == 'right' ? 'left' : 'top';
+    island.style[styleDirection] =
+      islandBoundingBox[axis] + 16 * increment + 'px';
 
+    var objs = document.querySelectorAll('.objs');
+    objs.forEach(obj => {
+      objPos = obj.getBoundingClientRect();
 
-      var move = function(direction) {
-
-        var islandBoundingBox = island.getBoundingClientRect();
-        var playerBoundingBox = playerElement.getBoundingClientRect();
-        var increment = direction == 'bottom' || direction == 'right' ? -1 : 1;
-        var axis = direction == 'top' || direction == 'bottom' ? 'y' : 'x';
-        var styleDirection =
-          direction == 'left' || direction == 'right' ? 'left' : 'top';
-        island.style[styleDirection] =
-          islandBoundingBox[axis] + 16 * increment + 'px';
-
-
-        var objs = document.querySelectorAll('.objs');
-        objs.forEach((obj) => {
-          objPos = obj.getBoundingClientRect();
-
-          if (
-            Math.abs(playerBoundingBox.x - objPos.x) <= 64 &&
-            Math.abs(playerBoundingBox.y - objPos.y) <= 64
-          ) {
-            if (obj.style.display == 'none') {
-
-            } else if (obj.dataset.type == 'food') {
-              addLife();
-              obj.style.display = 'none';
-            } else if (obj.dataset.type == 'tool') {
-              addToInvetory(obj);
-              obj.style.display = 'none';
-            }
-          } else {
-            //obj.style.backgroundColor = '';
-          }
-        });
-
-      };
-
-      function step(timestamp) {
-        if (timestamp - lastTimestamp > 50) {
-          lastTimestamp = timestamp;
-          //console.log('time...')
-          if (movement) {
-            move(movement);
-            //console.log(movement)
-          }
+      if (
+        Math.abs(playerBoundingBox.x - objPos.x) <= 64 &&
+        Math.abs(playerBoundingBox.y - objPos.y) <= 64
+      ) {
+        if (obj.style.display == 'none') {
+        } else if (obj.dataset.type == 'food') {
+          addLife();
+          obj.style.display = 'none';
+        } else if (obj.dataset.type == 'tool') {
+          addToInvetory(obj);
+          obj.style.display = 'none';
         }
-
-        window.requestAnimationFrame(step);
-
+      } else {
+        //obj.style.backgroundColor = '';
       }
+    });
+  };
 
-      window.requestAnimationFrame(step);
-      document.querySelector('#down').addEventListener('mousedown', () => {
-        //move('bottom');
-        movement = 'bottom';
-      });
-
-      window.addEventListener('mouseup', () => {
-        //move('bottom');
-        movement = null;
-      });
-
-      document.querySelector('#up').addEventListener('mousedown', () => {
-        movement = 'top';
-      });
-
-      document.querySelector('#left').addEventListener('mousedown', () => {
-        movement = 'left';
-      });
-
-      document.querySelector('#right').addEventListener('mousedown', () => {
-        movement = 'right';
-      });
-
-
-      window.addEventListener('keyup', function(event) {
-        movement = null;
-      })
-
-      window.addEventListener('keydown', function(event) {
-        if (event.keyCode == 38) {
-          movement = 'top';
-        } else if (event.keyCode == 40) {
-          movement = 'bottom';
-        } else if (event.keyCode == 39) {
-          movement = 'right';
-        } else if (event.keyCode == 37) {
-          movement = 'left';
-        }
-      });
-
-
-      var addToInvetory = function(element) {
-        var newInventoryElement = element.children[0].cloneNode(true);
-
-        document.querySelector('#inventory').appendChild(newInventoryElement)
+  function step(timestamp) {
+    if (timestamp - lastTimestamp > 50) {
+      lastTimestamp = timestamp;
+      //console.log('time...')
+      if (movement) {
+        move(movement);
+        //console.log(movement)
       }
+    }
 
-      function addLife() {
-        var life = document.getElementById('life');
-        var selectTool = document.getElementsByClassName('count')[0];
-        var numLife = life.childNodes;
+    window.requestAnimationFrame(step);
+  }
 
-        var newLife = document.createElement('span');
-        if (numLife.length <= 6) {
-          newLife.setAttribute('class', 'lifeBar R ');
-        } else if (numLife.length > 6 && numLife.length < 12) {
-          newLife.setAttribute('class', 'lifeBar LG ');
-        } else {
-          newLife.setAttribute('class', 'lifeBar G ');
-        }
+  window.requestAnimationFrame(step);
+  document.querySelector('#down').addEventListener('mousedown', () => {
+    //move('bottom');
+    movement = 'bottom';
+  });
 
-        console.log(life.children);
-        if (life.children.length < 15) life.appendChild(newLife);
-      }
+  window.addEventListener('mouseup', () => {
+    //move('bottom');
+    movement = null;
+  });
+
+  document.querySelector('#up').addEventListener('mousedown', () => {
+    movement = 'top';
+  });
+
+  document.querySelector('#left').addEventListener('mousedown', () => {
+    movement = 'left';
+  });
+
+  document.querySelector('#right').addEventListener('mousedown', () => {
+    movement = 'right';
+  });
+
+  window.addEventListener('keyup', function(event) {
+    movement = null;
+  });
+
+  window.addEventListener('keydown', function(event) {
+    if (event.keyCode == 38) {
+      movement = 'top';
+    } else if (event.keyCode == 40) {
+      movement = 'bottom';
+    } else if (event.keyCode == 39) {
+      movement = 'right';
+    } else if (event.keyCode == 37) {
+      movement = 'left';
+    }
+  });
+  var clock = document.getElementById('clock');
+  function currentTime() {
+    var date = new Date();
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+    var midday = 'AM';
+
+    midday = hour < 12 ? 'AM' : 'PM';
+    hour = hour == 0 ? 12 : hour > 12 ? hour - 12 : hour;
+
+    hour = updateTime(hour);
+    console.log(hour);
+    min = updateTime(min);
+    sec = updateTime(sec);
+    console.log(clock);
+    clock.innerHTML = hour + ' : ' + min + ' : ' + sec + ' ' + midday;
+    // var t = setTimeout(currentTime, 1000);
+  }
+
+  function updateTime(k) {
+    if (k < 10) {
+      return '0' + k;
+    } else {
+      return k;
+    }
+  }
+
+  currentTime();
+});
+var addToInvetory = function(element) {
+  var newInventoryElement = element.children[0].cloneNode(true);
+
+  document.querySelector('#inventory').appendChild(newInventoryElement);
+};
+
+function addLife() {
+  var life = document.getElementById('life');
+  var selectTool = document.getElementsByClassName('count')[0];
+  var numLife = life.childNodes;
+
+  var newLife = document.createElement('span');
+  if (numLife.length <= 6) {
+    newLife.setAttribute('class', 'lifeBar R ');
+  } else if (numLife.length > 6 && numLife.length < 12) {
+    newLife.setAttribute('class', 'lifeBar LG ');
+  } else {
+    newLife.setAttribute('class', 'lifeBar G ');
+  }
+
+  console.log(life.children);
+  if (life.children.length < 15) life.appendChild(newLife);
+}
